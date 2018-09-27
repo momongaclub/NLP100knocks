fb4d31be401f550304e647f8bbad656b43ac9a9f
Ryota_Mibayashi <threeforest8@gmail.com>
2018-09-27 21:27:59 +0900

    Update n03.py

--- n03.py
+++ n03.py
@@ -1,7 +1,6 @@
 string = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
 
 answer = []
-#正規表現で文字列しかとれないようにする
 string = string.replace(',','')
 string = string.replace('.','')
 
