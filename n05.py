8c9933fc6ac810f2b2b84605c0076bc8ab477c84
Ryota_Mibayashi <threeforest8@gmail.com>
2018-10-11 22:03:30 +0900

    Add files via upload

--- n05.py
+++ n05.py
@@ -1,5 +1,4 @@
 def seq2ngram(seq, n):
-
     seq_list = seq.split()
     word_bigram = [seq_list[i:i+n] for i in range(len(seq_list) - n + 1)]
     str_bigram = [seq[i:i+n] for i in range(len(seq) - n + 1)]
