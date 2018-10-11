781750dd04d8fd5dbc7611c7ae7c3d319dd3039e
Ryota_Mibayashi <threeforest8@gmail.com>
2018-10-11 22:02:31 +0900

    Add files via upload

---
+++ n05.py
@@ -0,0 +1,9 @@
+def seq2ngram(seq, n):
+
+    seq_list = seq.split()
+    word_bigram = [seq_list[i:i+n] for i in range(len(seq_list) - n + 1)]
+    str_bigram = [seq[i:i+n] for i in range(len(seq) - n + 1)]
+    print(word_bigram, str_bigram)
+
+
+seq2ngram("I am an NLPer", 2)
